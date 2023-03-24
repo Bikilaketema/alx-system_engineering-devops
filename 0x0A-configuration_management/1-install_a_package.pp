4af841baa4dcb6155cc00b44dca0bd5313b0bb44
bikilaketema <bikilaketema94@gmail.com>
2023-03-24 22:47:53 +0300

    trying task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -10,7 +10,7 @@ package { 'python3-pip':
 exec { 'install-flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
   path    => ['/usr/bin'],
-  unless  => '/usr/bin/pip3 freeze | /bin/grep flask==2.1.0',
+  unless  => '/usr/bin/pip3 freeze | /bin/grep -w flask==2.1.0',
   require => [Package['python3-pip'], Class['::python']],
 }
 
