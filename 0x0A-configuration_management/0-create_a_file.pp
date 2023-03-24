071a6e430d6f342c246c0c440d67fa70be18640e
bikilaketema <bikilaketema94@gmail.com>
2023-03-24 16:18:53 +0300

    again tried fixing an error in Task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 #Creates a file in /tmp/school with the ff parameters
+
 file { '/tmp/school':
- path    => '/tmp/school', 
  ensure  => file,
- content => 'I Love Puppet',
+ path    => '/tmp/school', 
  mode    => '0744',
  owner   => 'www-data',
  group   => 'www-data',
+ content => 'I Love Puppet'
 }
