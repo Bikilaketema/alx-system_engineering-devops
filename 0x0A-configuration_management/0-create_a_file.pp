d074017a8a0156e5c056e57dcf9ef05b619233ae
bikilaketema <bikilaketema94@gmail.com>
2023-03-24 16:35:10 +0300

    hope it works

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 #Creates a file in /tmp/school with the ff parameters
 file { '/tmp/school':
-   ensure  => file,
-   path    => '/tmp/school',
-   mode    => '0744',
-   owner   => 'www-data',
-   group   => 'www-data',
-   content => 'I Love Puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I Love Puppet'
 }
