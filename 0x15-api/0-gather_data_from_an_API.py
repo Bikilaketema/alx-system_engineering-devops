0f5776008fe6fcf3581c4ef6d6ab919357cf466e
bikilaketema <bikilaketema94@gmail.com>
2023-04-24 21:40:41 +0300

    Task 0- debug

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Retrieve TODO list progress of a given employee ID from a REST API."""
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
@@ -9,12 +9,14 @@ if __name__ == "__main__":
         user_id = sys.argv[1]
         api_url = "https://jsonplaceholder.typicode.com"
         employee = requests.get(api_url + "/users/{}".format(user_id)).json()
-        todo_list = requests.get(api_url + "/todos", params={"userId": user_id}).json()
+        todo_list =
+        requests.get(api_url + "/todos", params={"userId": user_id}).json()
 
         completed_tasks = [task for task in todo_list if task["completed"]]
         progress = "{}/{}".format(len(completed_tasks), len(todo_list))
 
-        print("Employee {} is done with tasks({}):".format(employee["name"], progress))
+        print("Employee {} is done with tasks({}):"
+              .format(employee["name"], progress))
         for task in completed_tasks:
             print("\t {}".format(task["title"]))
     else:
