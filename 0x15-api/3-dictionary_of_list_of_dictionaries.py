fef1047c9256df2fd187ec087e2fa4c55e40b47a
bikilaketema <bikilaketema94@gmail.com>
2023-04-24 22:36:48 +0300

    Task 3- done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,44 +1,44 @@
-#!/usr/bin/python3
-"""
-This module defines a function that retrieves all tasks from the given
-REST API endpoint and exports them to a JSON file.
-"""
-
 import json
 import requests
 from sys import argv
 
 
 def export_all_to_json():
-    """Exports all tasks from the given REST API endpoint to a JSON file."""
+    # Endpoint URLs for tasks and users
     url = 'https://jsonplaceholder.typicode.com/todos/'
     users_url = 'https://jsonplaceholder.typicode.com/users/'
 
+    # Retrieve tasks and users from the API
     tasks = requests.get(url).json()
     users = requests.get(users_url).json()
 
+    # Check if tasks are empty
     if not tasks:
         print("No tasks found")
         return
 
-    # create a dictionary to hold tasks for all users
+    # Create a dictionary to hold tasks for all users
     tasks_by_user = {}
 
+    # Populate the dictionary with empty lists for each user
     for user in users:
         user_id = user['id']
-        user_name = user['username']
         tasks_by_user[user_id] = []
 
-    # populate the dictionary with tasks for all users
+    # Populate the dictionary with tasks for all users
     for task in tasks:
         user_id = task['userId']
         task_dict = {}
+
+        # Add task details to a dictionary
         task_dict['username'] = [user['username'] for user in users if user['id'] == user_id][0]
         task_dict['task'] = task['title']
         task_dict['completed'] = task['completed']
+
+        # Append the task dictionary to the list of tasks for the user
         tasks_by_user[user_id].append(task_dict)
 
-    # write the dictionary to a JSON file
+    # Write the dictionary to a JSON file
     with open("todo_all_employees.json", 'w') as file:
         json.dump(tasks_by_user, file)
 
