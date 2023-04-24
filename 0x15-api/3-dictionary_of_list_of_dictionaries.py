55b1c42e057bd474a73944896889e7aa8c94b654
bikilaketema <bikilaketema94@gmail.com>
2023-04-24 22:49:44 +0300

    Task 3- done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,17 +5,28 @@ Export data from the JSONPlaceholder API to JSON format.
 
 import json
 import requests
+import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
+if __name__ == '__main__':
+    api_url = 'https://jsonplaceholder.typicode.com'
+    users = requests.get(api_url + '/users').json()
+    tasks = requests.get(api_url + '/todos').json()
 
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    # Create dictionary to store tasks for each user
+    user_tasks = {}
+    for user in users:
+        user_id = user['id']
+        user_name = user['username']
+        user_tasks[user_id] = []
+        for task in tasks:
+            if task['userId'] == user_id:
+                task_dict = {
+                    'username': user_name,
+                    'task': task['title'],
+                    'completed': task['completed']
+                }
+                user_tasks[user_id].append(task_dict)
+
+    # Write data to file in JSON format
+    with open('todo_all_employees.json', mode='w') as file:
+        json.dump(user_tasks, file)
