c999cc1f5288c37190befd4658bb33efee0c052c
bikilaketema <bikilaketema94@gmail.com>
2023-04-24 22:02:26 +0300

    Task 1- debug

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,33 +1,31 @@
 #!/usr/bin/python3
-"""
-Python script that, using this REST API, for a given employee ID, returns
-information about his/her TODO list progress and export data in the CSV format.
-"""
+"""Script that, using a given REST API, for a given employee ID,
+returns information about his/her TODO list progress, and exports it to CSV."""
+
 import csv
 import requests
 from sys import argv
 
 
-if __name__ == '__main__':
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(argv[1])
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(argv[1])
-    response = requests.get(url)
+if __name__ == "__main__":
+    # Get user id from command line arguments
+    user_id = argv[1]
+    # Get user info
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     user_response = requests.get(user_url)
-    todos = response.json()
-    user = user_response.json()
-
-    with open('{}.csv'.format(argv[1]), mode='w') as csv_file:
-        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for todo in todos:
-            writer.writerow([user['id'], user['username'], todo['completed'], todo['title']])
-
-    task_total = len(todos)
-    completed_task = sum(task['completed'] for task in todos)
-
-    print("Employee {} is done with tasks({}/{}):".format(user['name'], completed_task, task_total))
-    for task in todos:
-        if task['completed'] is True:
-            print("\t {}".format(task['title']))
-
+    user_data = user_response.json()
+    # Get user's todo list
+    todo_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
+    todo_response = requests.get(todo_url)
+    todo_data = todo_response.json()
+    # Export data to CSV file
+    file_name = "{}.csv".format(user_id)
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for todo in todo_data:
+            task_completed_status = str(todo['completed'])
+            task_title = todo['title']
+            writer.writerow([user_id, user_data['username'], task_completed_status, task_title])
+    # Print feedback message
     print("Number of tasks in CSV: OK")
 
