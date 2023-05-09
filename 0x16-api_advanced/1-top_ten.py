c88f6e53688cd7d9f33ab1e6fb6a856f74a36198
bikilaketema <bikilaketema94@gmail.com>
2023-05-09 18:42:49 +0300

    debug

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,35 @@
 #!/usr/bin/python3
 """
 Queries the Reddit API and prints the titles
-of the first 10 hot posts for a given subreddit.
+of the first 10 hot posts listed for a given subreddit.
 """
+
 
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot
-    posts for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json".format(subreddit)"
-    headers = {"User-Agent": "0x16-api_advanced: project: \
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
 
-    }
-    params = {
-         "limit": 10
+    Args:
+        subreddit: Name of the subreddit to fetch the top 10 hot posts for.
 
-    }
-
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    Returns:
+        None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "Bikila/5.0 (Bicky; Biko; ket)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 400:
-        print(None)
+        print("None")
         return
 
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+    for post in response.json()["data"]["children"]:
+        print(post["data"]["title"])
+
+
+if __name__ == "__main__":
+    subreddit = input("Enter subreddit name: ")
+    top_ten(subreddit)
