47d5dc0831daf5b448bd7779c3f5ec1f95b62c9b
bikilaketema <bikilaketema94@gmail.com>
2023-05-09 14:17:47 +0300

    Debug task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,7 +13,7 @@ def number_of_subscribers(subreddit):
     headers = {"User-Agent": "myBot/0.0.1"}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
+    if response.status_code == 404:
         return 0
 
     data = response.json()
