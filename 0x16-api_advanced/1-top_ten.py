089eddde831d8a7734e3472045d5e70d0e72683b
bikilaketema <bikilaketema94@gmail.com>
2023-05-09 18:09:20 +0300

    Task 1 done

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,8 +10,15 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "myBot/0.0.1"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {"User-Agent": "0x16-api_advanced: pro: \}
+    params = {
+
+         "limit": 10
+
+    }
+
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
 
     if response.status_code != 200:
         print(None)
