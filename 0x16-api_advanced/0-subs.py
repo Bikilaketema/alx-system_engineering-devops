7bcf0b9985a46622fdac794dcd474569189ec881
bikilaketema <bikilaketema94@gmail.com>
2023-05-09 17:58:34 +0300

    Debug

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,8 +11,9 @@ def number_of_subscribers(subreddit):
     """Retrieves the number of subscribers for a given subreddit."""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    url = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
+    url = requests.get('http://www.reddit.com/r/{}/about.json'
+                       .format(subreddit),
+                       headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /b/bicky)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
+    subs = url.get("data", {}).get("subscribers", 0)
     return subs
