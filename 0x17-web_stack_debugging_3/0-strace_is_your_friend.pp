fdb3856a1da8b916b3efcbf2282f34da22c676bf
bikilaketema <bikilaketema94@gmail.com>
2023-05-10 14:44:44 +0300

    Debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,9 +1,5 @@
 # Puppet manifest to fix a 500 error in WordPress by replacing 'phpp' with 'php' in wp-settings.php file.
 
-# This manifest uses an 'exec' resource to run the 'sed' command with the specified options. 
-# The 'command' parameter specifies the command to be executed, which replaces all occurrences of 'phpp' with 'php' in the wp-settings.php file. 
-# The 'path' parameter specifies the location of the sed command.
-
 exec { 'fixed-phpp':
   command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin';
